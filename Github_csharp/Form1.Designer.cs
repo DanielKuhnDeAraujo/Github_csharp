a518160194de6fe050f08778a9a6cb28ae47800c
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-03-02 11:27:24 -0300

    colocando os boytõe

--- Github_csharp/Form1.Designer.cs
+++ Github_csharp/Form1.Designer.cs
@@ -36,25 +36,30 @@
             this.lbl_alg = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lbl_res = new System.Windows.Forms.Label();
+            this.btn_sub = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtBox1
             // 
-            this.txtBox1.Location = new System.Drawing.Point(57, 30);
+            this.txtBox1.Location = new System.Drawing.Point(151, 30);
             this.txtBox1.Name = "txtBox1";
             this.txtBox1.Size = new System.Drawing.Size(141, 26);
             this.txtBox1.TabIndex = 0;
             // 
             // txtBox2
             // 
-            this.txtBox2.Location = new System.Drawing.Point(57, 90);
+            this.txtBox2.Location = new System.Drawing.Point(151, 63);
             this.txtBox2.Name = "txtBox2";
             this.txtBox2.Size = new System.Drawing.Size(141, 26);
             this.txtBox2.TabIndex = 0;
             // 
             // btn_fechar
             // 
-            this.btn_fechar.Location = new System.Drawing.Point(362, 193);
+            this.btn_fechar.Location = new System.Drawing.Point(429, 265);
             this.btn_fechar.Name = "btn_fechar";
             this.btn_fechar.Size = new System.Drawing.Size(141, 47);
             this.btn_fechar.TabIndex = 1;
@@ -63,7 +68,7 @@
             // 
             // btn_limpar
             // 
-            this.btn_limpar.Location = new System.Drawing.Point(57, 193);
+            this.btn_limpar.Location = new System.Drawing.Point(48, 265);
             this.btn_limpar.Name = "btn_limpar";
             this.btn_limpar.Size = new System.Drawing.Size(141, 47);
             this.btn_limpar.TabIndex = 1;
@@ -73,7 +78,7 @@
             // 
             // btn_somar
             // 
-            this.btn_somar.Location = new System.Drawing.Point(362, 20);
+            this.btn_somar.Location = new System.Drawing.Point(429, 20);
             this.btn_somar.Name = "btn_somar";
             this.btn_somar.Size = new System.Drawing.Size(141, 47);
             this.btn_somar.TabIndex = 1;
@@ -85,7 +90,7 @@
             // 
             this.lbl_alg.AutoSize = true;
             this.lbl_alg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lbl_alg.Location = new System.Drawing.Point(32, 90);
+            this.lbl_alg.Location = new System.Drawing.Point(125, 66);
             this.lbl_alg.Name = "lbl_alg";
             this.lbl_alg.Size = new System.Drawing.Size(20, 22);
             this.lbl_alg.TabIndex = 2;
@@ -95,7 +100,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 112);
+            this.label2.Location = new System.Drawing.Point(147, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(153, 20);
             this.label2.TabIndex = 2;
@@ -105,19 +110,69 @@
             // lbl_res
             // 
             this.lbl_res.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lbl_res.Location = new System.Drawing.Point(45, 143);
+            this.lbl_res.Location = new System.Drawing.Point(151, 121);
             this.lbl_res.Name = "lbl_res";
-            this.lbl_res.Size = new System.Drawing.Size(153, 29);
+            this.lbl_res.Size = new System.Drawing.Size(141, 29);
             this.lbl_res.TabIndex = 3;
             this.lbl_res.Text = "?";
             this.lbl_res.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_sub
+            // 
+            this.btn_sub.Location = new System.Drawing.Point(429, 79);
+            this.btn_sub.Name = "btn_sub";
+            this.btn_sub.Size = new System.Drawing.Size(141, 47);
+            this.btn_sub.TabIndex = 4;
+            this.btn_sub.Text = "Sub&trair";
+            this.btn_sub.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "&Multiplicação";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(429, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 47);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "&Dividir";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(81, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 47);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "&Comparar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(228, 172);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 47);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "&Par Ímpr";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(532, 262);
+            this.ClientSize = new System.Drawing.Size(595, 324);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_sub);
             this.Controls.Add(this.lbl_res);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbl_alg);
@@ -143,6 +198,11 @@
         private System.Windows.Forms.Label lbl_alg;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lbl_res;
+        private System.Windows.Forms.Button btn_sub;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
