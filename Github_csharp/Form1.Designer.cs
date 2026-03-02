d23b149de65b6661dd2dd0a8e5cae31ed0c0480a
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-03-02 11:28:07 -0300

    só ajeitado um nome

--- Github_csharp/Form1.Designer.cs
+++ Github_csharp/Form1.Designer.cs
@@ -159,7 +159,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(141, 47);
             this.button4.TabIndex = 8;
-            this.button4.Text = "&Par Ímpr";
+            this.button4.Text = "&Par Ímpar";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
