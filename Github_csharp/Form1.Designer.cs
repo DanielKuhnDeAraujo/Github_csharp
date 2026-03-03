7bd8533e54dbce975c4114daa508f2854b91c739
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-03-03 11:24:53 -0300

    Calculadora finalizada

--- Github_csharp/Form1.Designer.cs
+++ Github_csharp/Form1.Designer.cs
@@ -34,32 +34,35 @@
             this.btn_limpar = new System.Windows.Forms.Button();
             this.btn_somar = new System.Windows.Forms.Button();
             this.lbl_alg = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.lbl_res = new System.Windows.Forms.Label();
             this.btn_sub = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.titulo = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtBox1
             // 
-            this.txtBox1.Location = new System.Drawing.Point(151, 30);
+            this.txtBox1.Location = new System.Drawing.Point(157, 106);
             this.txtBox1.Name = "txtBox1";
-            this.txtBox1.Size = new System.Drawing.Size(141, 26);
+            this.txtBox1.Size = new System.Drawing.Size(138, 26);
             this.txtBox1.TabIndex = 0;
+            this.txtBox1.TextChanged += new System.EventHandler(this.txtBox1_TextChanged);
             // 
             // txtBox2
             // 
-            this.txtBox2.Location = new System.Drawing.Point(151, 63);
+            this.txtBox2.Location = new System.Drawing.Point(157, 146);
             this.txtBox2.Name = "txtBox2";
-            this.txtBox2.Size = new System.Drawing.Size(141, 26);
+            this.txtBox2.Size = new System.Drawing.Size(138, 26);
             this.txtBox2.TabIndex = 0;
+            this.txtBox2.TextChanged += new System.EventHandler(this.txtBox2_TextChanged);
             // 
             // btn_fechar
             // 
-            this.btn_fechar.Location = new System.Drawing.Point(429, 265);
+            this.btn_fechar.Location = new System.Drawing.Point(454, 303);
             this.btn_fechar.Name = "btn_fechar";
             this.btn_fechar.Size = new System.Drawing.Size(141, 47);
             this.btn_fechar.TabIndex = 1;
@@ -69,7 +72,7 @@
             // 
             // btn_limpar
             // 
-            this.btn_limpar.Location = new System.Drawing.Point(48, 265);
+            this.btn_limpar.Location = new System.Drawing.Point(10, 303);
             this.btn_limpar.Name = "btn_limpar";
             this.btn_limpar.Size = new System.Drawing.Size(141, 47);
             this.btn_limpar.TabIndex = 1;
@@ -79,7 +82,7 @@
             // 
             // btn_somar
             // 
-            this.btn_somar.Location = new System.Drawing.Point(429, 20);
+            this.btn_somar.Location = new System.Drawing.Point(454, 85);
             this.btn_somar.Name = "btn_somar";
             this.btn_somar.Size = new System.Drawing.Size(141, 47);
             this.btn_somar.TabIndex = 1;
@@ -91,36 +94,27 @@
             // 
             this.lbl_alg.AutoSize = true;
             this.lbl_alg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lbl_alg.Location = new System.Drawing.Point(125, 66);
+            this.lbl_alg.Location = new System.Drawing.Point(105, 150);
             this.lbl_alg.Name = "lbl_alg";
             this.lbl_alg.Size = new System.Drawing.Size(20, 22);
             this.lbl_alg.TabIndex = 2;
             this.lbl_alg.Text = "?";
             this.lbl_alg.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(147, 92);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(153, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "________________";
-            this.label2.Click += new System.EventHandler(this.label1_Click);
-            // 
             // lbl_res
             // 
             this.lbl_res.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lbl_res.Location = new System.Drawing.Point(151, 121);
+            this.lbl_res.Location = new System.Drawing.Point(157, 209);
             this.lbl_res.Name = "lbl_res";
-            this.lbl_res.Size = new System.Drawing.Size(141, 29);
+            this.lbl_res.Size = new System.Drawing.Size(141, 69);
             this.lbl_res.TabIndex = 3;
             this.lbl_res.Text = "?";
             this.lbl_res.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_res.Click += new System.EventHandler(this.lbl_res_Click);
             // 
             // btn_sub
             // 
-            this.btn_sub.Location = new System.Drawing.Point(429, 79);
+            this.btn_sub.Location = new System.Drawing.Point(454, 144);
             this.btn_sub.Name = "btn_sub";
             this.btn_sub.Size = new System.Drawing.Size(141, 47);
             this.btn_sub.TabIndex = 4;
@@ -130,7 +124,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(429, 132);
+            this.button1.Location = new System.Drawing.Point(454, 197);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 47);
             this.button1.TabIndex = 5;
@@ -140,7 +134,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(429, 185);
+            this.button2.Location = new System.Drawing.Point(454, 250);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(141, 47);
             this.button2.TabIndex = 6;
@@ -150,7 +144,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(81, 172);
+            this.button3.Location = new System.Drawing.Point(157, 303);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(141, 47);
             this.button3.TabIndex = 7;
@@ -158,28 +152,50 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Stencil", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(55, 22);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(377, 61);
+            this.titulo.TabIndex = 9;
+            this.titulo.Text = "Calculadora";
+            this.titulo.Click += new System.EventHandler(this.label1_Click_1);
+            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(228, 172);
+            this.button4.Location = new System.Drawing.Point(304, 303);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(141, 47);
             this.button4.TabIndex = 8;
             this.button4.Text = "&Par Ímpar";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "________________";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(595, 324);
+            this.ClientSize = new System.Drawing.Size(625, 362);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titulo);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_sub);
             this.Controls.Add(this.lbl_res);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.lbl_alg);
             this.Controls.Add(this.btn_somar);
             this.Controls.Add(this.btn_limpar);
@@ -187,7 +203,7 @@
             this.Controls.Add(this.txtBox2);
             this.Controls.Add(this.txtBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calculadora";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,13 +217,14 @@
         private System.Windows.Forms.Button btn_limpar;
         private System.Windows.Forms.Button btn_somar;
         private System.Windows.Forms.Label lbl_alg;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lbl_res;
         private System.Windows.Forms.Button btn_sub;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label titulo;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Github_csharp/Form1.cs
+++ Github_csharp/Form1.cs
@@ -13,6 +13,12 @@ namespace Github_csharp
 {
     public partial class Form1 : Form
     {
+        // utilidades
+        public bool Par(int num)
+        {
+            return (num % 2 == 0);
+        }
+        // código mesmo
         public Form1()
         {
             InitializeComponent();
@@ -42,7 +48,11 @@ namespace Github_csharp
 
         private void btn_limpar_Click(object sender, EventArgs e)
         {
-            Close();
+            txtBox1.Text = "";
+            txtBox2.Text = "";
+            lbl_res.Text = "";
+            lbl_alg.Text = "";
+
         }
 
         private void btn_sub_Click(object sender, EventArgs e)
@@ -119,12 +129,12 @@ namespace Github_csharp
                     if (a < b)
                     {
                         lbl_alg.Text = "<";
-                        lbl_res.Text = $"{a} é o maior";
+                        lbl_res.Text = $"{b} é o maior";
                     }
                     else
                     {
                         lbl_alg.Text = ">";
-                        lbl_res.Text = $"{b} é o maior";
+                        lbl_res.Text = $"{a} é o maior";
                     }
                 }
                 else
@@ -137,6 +147,57 @@ namespace Github_csharp
             {
                 MessageBox.Show("Informe apenas números");
             }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lbl_alg.Text = "%";
+            lbl_res.Text = "";
+            decimal a, b;
+            try
+            {
+                a = decimal.Parse(txtBox1.Text);
+                b = decimal.Parse(txtBox2.Text);
+                if (Par((int)a)) {
+                    lbl_res.Text = $"{a}: par";
+                }
+                else
+                {
+                    lbl_res.Text = $"{a}: ímpar";
+                }
+                if (Par((int)b))
+                {
+                    lbl_res.Text += $"\n{b}: par";
+                }
+                else
+                {
+                    lbl_res.Text += $"\n{b}: ímpar";
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Informe apenas números");
+            }
+        }
+
+        private void lbl_res_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
